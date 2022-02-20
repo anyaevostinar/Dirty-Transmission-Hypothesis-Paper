6f2f3c190182b656f11f8ad7fa5e164055cb7dda
Anya E. Vostinar <anya.vostinar@gmail.com>
2022-02-20 12:39:25 -0600

    munged data for low int val mutation rate

---
+++ Analysis/02.13.2022-NoIntValMut/munge_efficiency.py
@@ -0,0 +1,30 @@
+import os.path
+import gzip
+
+folder = "../../Data/02.13.2022-NoIntValMut/"
+
+treatment_postfixes_HMR = ["HMR0.1", "HMR0.5", "HMR1.0"]
+vert_postfixes = ["VR0.1", "VR0.2", "VR0.3", "VR0.4", "VR0.5", "VR0.6", "VR0.7", "VR0.8", "VR0.9", "VR1.0"]
+reps = range(10, 40)
+header = "uid HMR VR rep update efficiency\n"
+
+outputFileName = "munged_efficiency.dat"
+
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for t in treatment_postfixes_HMR:
+    for v in vert_postfixes:
+        for r in reps:
+
+            fname = folder +"Efficiency"+t+v+"_Seed" + str(r) +"_SEED"+str(r)+ ".data"
+            uid = t + "_" + str(r)
+            curFile = open(fname, 'r')
+            for line in curFile:
+                if (line[0] != "u"):
+                    splitline = line.split(',')
+                    #if int(splitline[0]) == final_update:
+                    outstring1 = "{} {} {} {} {} {}".format(uid, t, v, r, splitline[0], splitline[1])
+                    outFile.write(outstring1)
+            curFile.close()
+outFile.close()
---
+++ Analysis/02.13.2022-NoIntValMut/munge_histograms_sym.py
@@ -0,0 +1,71 @@
+import os.path
+import gzip
+
+folder = '../../Data/02.13.2022-NoIntValMut/'
+
+treatment_prefixes = ["HMR0.1VR0.3", "HMR0.5VR0.3", "HMR1.0VR0.3"]
+#treatment_prefixes = ["HMR0.1VR0.4", "HMR0.5VR0.4", "HMR1.0VR0.4"]
+mut_sizes = ['']
+reps = range(10,40)
+p="Sym"
+
+#reps = range(1001, 1021)
+header = "uid treatment rep update interval count partner\n"
+
+outputFileName = "munged_histogram_sym_vt30.dat"
+
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for t in treatment_prefixes:
+    for r in reps:
+        for m in mut_sizes:
+            fname = folder +p+"Vals"+t+"_Seed" + str(r) +"_SEED"+str(r) + ".data"
+            uid = t+m + "_" + str(r)
+            curFile = open(fname, 'r')
+            for line in curFile:
+                if (line[0] != "u"):
+                    splitline = line.strip().split(',')
+                    
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-1_-.9", splitline[3], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.9_-.8", splitline[4], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.8_-.7", splitline[5], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.7_-.6", splitline[6], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.6_-.5", splitline[7], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.5_-.4", splitline[8], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.4_-.3", splitline[9], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.3_-.2", splitline[10], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.2_-.1", splitline[11], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "-.1_0", splitline[12], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], "0_.1", splitline[13], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".1_.2", splitline[14], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".2_.3", splitline[15], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".3_.4", splitline[16], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".4_.5", splitline[17], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".5_.6", splitline[18], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".6_.7", splitline[19], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".7_.8", splitline[20], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".8_.9", splitline[21], p)
+                    outFile.write(outstring1)
+                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t+m, r, splitline[0], ".9_1", splitline[22], p)
+                    outFile.write(outstring1)
+            curFile.close()
+outFile.close()
